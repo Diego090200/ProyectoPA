a930b943f8ee50e0c4d700b46eae69adef6ed000
ArgeReyes <44539912+ArgeReyes@users.noreply.github.com>
2018-10-28 14:29:41 -0600

    Update proyectop1.rb

--- proyectop1.rb
+++ proyectop1.rb
@@ -1,1 +1,54 @@
-#proyecto prueba 1
+#proyecto prueba 1   puts "BOOKSTORE"
+
+puts "Seleccione una opción: "
+puts "1. Administración de libros"
+puts "2. Control de ventas:"
+
+opcion=gets.chomp.to_i
+
+if opcion==1
+    puts "Seleccione la acción que desea"
+    puts "a. Registro de nuevos libros" 
+    puts "b. Registro de autores"
+    puts "c. Listado de libros"
+    puts "d. Listado de autores"
+    puts "e. Buscar libro"
+    puts "f. Buscar autor" 
+
+    opcion1= gets.chomp.downcase
+    if opcion1=='a'
+        registrol()
+    elsif opcion1=='b'
+        registroa()
+    elsif opcion1=='c'
+        listadol()
+    elsif opcion1=='d'
+        listadoa()
+    elsif opcion1=='e'
+        buscarl()
+    elsif opcion1=='f'
+        buscara()
+    else
+        puts "Ingreso una opción inválida"
+    end
+
+elsif opcion==2
+    puts "Seleccione la acción que desea"
+    puts "a. Registrar una venta" 
+    puts "b. Buscar una venta"
+    puts "c. Listado de ventas"
+
+    opcion2= gets.chomp.downcase
+
+    if opcion2=='a'
+        registrov()
+    elsif opcion2=='b'
+        buscarv()
+    elsif opcion2=='c'
+        listadov()
+    else
+        puts "Ingreso una opción inválida"
+    end
+else 
+    puts "Ingreso un valor inválido"
+end
